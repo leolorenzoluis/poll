bd703bf943720cbbdbbc20c3c9be9360d4de32f4
Leo Lorenzo Luis <leolorenzoluis@outlook.com>
2016-01-03 19:35:28 +0000

    Enabled sockets and integrate with OL

---
+++ web/channels/poll_channel.ex
@@ -0,0 +1,41 @@
+defmodule Poll.PollChannel do
+  use Phoenix.Channel
+  require Logger
+  require RethinkDB.Lambda
+  import RethinkDB.Query
+  import Poll.Database
+
+  def join("eleksyon:lobby", message, socket) do
+    send(self, :after_join)
+    {:ok, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    IO.puts "im in handle info"
+
+    query = db("poll") 
+  	|> table("geo") 
+  	|> pluck("location") 
+  	|> map( RethinkDB.Lambda.lambda fn (record) -> record[:location] |> to_geojson end) 
+    result = run(query)
+    IO.inspect result
+    Enum.each(result.data, fn message ->  push socket, "new:msg", message end)
+
+    changes = changes(query)
+    |> run
+
+    Task.async fn ->
+      Enum.each(changes, fn change ->
+    	IO.inspect changes
+        push socket, "new:msg", change["new_val"]
+      end)
+    end
+
+    {:noreply, socket}
+  end
+
+  def handle_in("new:msg", msg, socket) do
+    #TODO: handle add/updates?
+    {:reply, {:ok, msg["coordinates"]}, assign(socket, :user, msg["type"])}
+  end
+end
