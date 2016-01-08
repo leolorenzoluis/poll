a9641ec2acd740e337fdb3e205db56e78c781338
Leo Lorenzo Luis <leolorenzoluis@outlook.com>
2016-01-08 00:39:08 +0000

    Started scaffolding workflow

--- web/controllers/vote_controller.ex
+++ web/controllers/vote_controller.ex
@@ -1,17 +1,42 @@
 defmodule Poll.VoteController do
   use Poll.Web, :controller
+  import RethinkDB.Query
 
   def index(conn, _params) do
     currentUser = get_session(conn, :current_user)
   	if(is_nil(currentUser)) do
       conn
-      |> put_flash(:error, "Failed to authenticate.")
+      |> put_flash(:error, "You must sign in to your Facebook account in order to vote.")
       |> redirect(to: "/")
   	else
-  		conn 
-  		|> put_flash(:error, "Time to vote papi.")
-  		|> redirect(to: "/")
+      result = db("poll") 
+      |> table("presidents") 
+      |> Poll.Database.run
+      
+
+      split = Enum.chunk(result.data, 3,3,[nil,nil,nil])
+
+      Enum.each(split, fn (x) -> Enum.each(x, fn (y) -> IO.inspect y["firstname"] end) end)
+
+      test =hd (hd(split))
+      #IO.inspect test
+      #IO.inspect test["firstname"]
+      val = Poison.encode split
+      conn 
+      |> assign(:current_user, currentUser)
+      |> assign(:presidents, split)
+      |> render("index.html")
+      
     end
-  	
+  end
+
+  def create(conn, _params) do
+    IO.puts "hello created!"
+
+    conn 
+      |> assign(:current_user, nil)
+      |> assign(:presidents, [])
+      |> render("index.html")
+
   end
 end
