e50a49c634ff3e143dc5ebbb398d478b2e590d30
Ubuntu <leo@ubuntuws.ubuntuws-s7638bee.b7.internal.cloudapp.net>
2016-02-02 03:19:03 +0000

    Created votes repo

--- web/controllers/presidents_controller.ex
+++ web/controllers/presidents_controller.ex
@@ -1,6 +1,7 @@
 defmodule Poll.PresidentsController do
   use Poll.Web, :controller
   import RethinkDB.Query
+  import Poll.VoteRepository
 
   def index(conn, _params) do
     currentUser = get_session(conn, :current_user)
@@ -14,54 +15,19 @@ defmodule Poll.PresidentsController do
       |> filter(%{Type: "President"})
       |> Poll.Database.run
       
-
-      split = Enum.chunk(result.data, 3,3,[nil,nil,nil])
-
-      Enum.each(split, fn (x) -> Enum.each(x, fn (y) -> IO.inspect y["firstname"] end) end)
-
-      test =hd (hd(split))
-      #IO.inspect test
-      #IO.inspect test["firstname"]
-      #val = Poison.encode split
       conn 
       |> assign(:current_user, currentUser)
-      |> assign(:candidates, split)
+      |> assign(:candidates, result)
       |> render("index.html")
     end
   end
 
   def create(conn, _params) do
+    IO.inspect _params["test"]
 
     currentUser = get_session(conn, :current_user)
-    longitudeTuple = Float.parse _params["longitude"] 
-    latitudeTuple = Float.parse _params["latitude"]
-    candidate = _params["candidate"] 
-    long = elem(longitudeTuple,0)
-    lat = elem(latitudeTuple,0)
-
-    result = db("poll") 
-    |> table("cities") 
-    |> get_nearest(point({long,lat}), %{index: "Location", max_dist: 5000})
-    |> Poll.Database.run
-
-    IO.puts "CREATE CREATE CREATE"
-    IO.inspect result.data
-    blah = is_list result.data
-    IO.inspect blah
-    if List.first(result.data) do
-      currentCity = hd(result.data)["doc"]["City"]
-
-      db("poll") 
-              |> table("users")
-              |> update(%{president: candidate})
-              |> Poll.Database.run
-
-      db("poll")
-      |> table("votes")
-      |> insert(%{city: currentCity, userid: currentUser.id, candidate: candidate, position: "president"})
-      |> Poll.Database.run
-    end
-
+    create_vote(currentUser, _params)
+   
     conn 
       |> put_flash(:info, "Successfully authenticated.")
       |> assign(:current_user, nil)
